5d65469331da5fba44614e44860ead24a450b00e
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-01 21:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function moveToNextInput(input) {
+            const nextInput = input.nextElementSibling;
+            if (input.value && nextInput) {
+                nextInput.focus();
+            }
+        }
 
+        document.querySelectorAll('.code').forEach(input => {
+            input.addEventListener('keydown', e => {
+                if (e.key === 'Backspace' && !input.value) {
+                    const prevInput = input.previousElementSibling;
+                    if (prevInput) {
+                        prevInput.focus();
+                    }
+                }
+            });
+        });
