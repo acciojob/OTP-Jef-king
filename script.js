11ae8526e2f716e9658c286e8665e47f493bfb6c
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-04-05 00:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function moveToNextInput(input) {
-            let nextInput = input.nextElementSibling;
-            if (input.value && nextInput) {
-                nextInput.focus();
+const codes = document.querySelectorAll(".code")
+
+codes[0].focus()
+
+codes.forEach((code,idx)=>{
+    code.addEventListener("keydown",(e)=>{
+        console.log(e.key)
+        const keyPressed = e.key
+
+        if(keyPressed>=0 && keyPressed<=9){
+            codes[idx].value=''
+            if(idx<codes.length-1){
+                setTimeout(()=>codes[idx+1].focus(),10)
             }
         }
-
-        document.querySelectorAll('.code').forEach(input => {
-            input.addEventListener('keydown', e => {
-                if (e.key === 'Backspace' && !input.value) {
-                    let prevInput = input.previousElementSibling;
-                    if (prevInput) {
-                        prevInput.focus();
-                    }
-                }
-            });
-        });
+        else if(keyPressed==="Backspace"){
+            if(idx>0){
+                setTimeout(()=>codes[idx-1].focus(),10)
+            }
+        }
+    })
+})
